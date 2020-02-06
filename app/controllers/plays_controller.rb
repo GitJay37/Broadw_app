ffa99cbf4a3b5e5bc1b14f40729f15e3e277ec07
GitJay37 <jeisondejesus@hotmail.es>
2020-02-06 14:01:31 -0500

    no funciona simple_form_for

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,4 +2,17 @@ class PlaysController < ApplicationController
     def index
     
     end
+
+    def new
+        @play = Play.new
+    end
+    
+    def create
+        @play = play_params
+    end
+
+    def play_params
+        params.require(:play).permit(:title, :description, :director)
+    end
+
 end
