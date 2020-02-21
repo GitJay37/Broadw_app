354b7ccbe18300719ca2c0ca19ebb3aa1c114794
Jeison De Jesús Anillo Pérez <Jeisondejesus@hotmail.es>
2020-02-20 21:23:42 -0500

    ratings pending

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,52 @@
 class ReviewsController < ApplicationController
-    before_action :find_play  
+    before_action :find_play 
+    before_action :find_review, only: [:edit, :update, :destroy]
 
     def new
-        @play = Play.find(params[:play_id])
         @review = Review.new
     end
 
     def create
         @review = Review.new(review_params)
-        @review.play_id = @play_id
-        @review.user_id = current_user.id
+		@review.play_id = @play.id
+		@review.user_id = current_user.id
 
-        if @review.save
-            redirect_to play_path(@play)
-        else 
-            render 'new'
-        end 
+		if @review.save
+			redirect_to play_path(@play)
+		else
+			render 'new'
+		end
     end
 
+    def edit
+
+    end
+
+    def update
+        
+        if @review.update(review_params)
+            redirect_to play_path(@play)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+		@review.destroy
+		redirect_to play_path(@play)
+	end
+
     private
+
         def review_params
-            paramas.require(:review).permit(:rating, comment)
+            params.require(:review).permit(:rating, :comment)
         end
 
         def find_play
             @play = Play.find(params[:play_id])
+        end
+
+        def find_review
+            @review = Review.find(params[:id])
         end
 end
